2b43a38f93fc40eed3ab48fe038438d5ec54400c
karlaDeniseGuerreroRangel <124050167@upq.edu.mx>
2025-09-04 23:23:19 -0600

    Actividad 2

---
+++ Actividad2/Actividad2.java
@@ -0,0 +1,15 @@
+public class inicioPAM{
+    public String ReglamentoPOO(){
+        return "Reglamneto POO"
+    } 
+    public String LineamientosClassroom(){
+        return "Lineamientos Classroom"
+    } 
+     public String Fechasdeparciales(){
+        return "Fechas de parciales"
+    } 
+     public String PorcentajeporParcial(){
+        return "Porcentaje por Parcial"
+    } 
+    
+}
