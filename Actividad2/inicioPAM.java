f8ea748026ea51ea5f19d5aba3543cc504cfba40
karlaDeniseGuerreroRangel <124050167@upq.edu.mx>
2025-09-04 23:30:56 -0600

    Actividad 2

---
+++ Actividad2/inicioPAM.java
@@ -0,0 +1,63 @@
+public class inicioPAM{
+    public static void main(String[] args){
+        System.out.println("Programación de aplicaciones móviles");
+         System.out.println("1.Reglamento POO");
+          System.out.println("2.Lineamientos Classroom");
+           System.out.println("3.Fechas de Parciales");
+            System.out.println("4.Porcentajes por Parcial");
+
+             System.out.println("Dijte la opcion:");
+            int opcion=scanner.nextInt();
+
+            switch(opcion){
+                case 1:
+                ReglamentoPOO();
+                break;
+                case 2:
+                 LineamientosClassroom();
+                break;
+                case 2:
+                 FechasdeParciales();
+                break;
+                case 2:
+                 PorcentajesporParcial();
+                break;
+            }
+       
+    } 
+    static void ReglamentoPOO(){
+        System.out.println("Reglamento POO");
+         System.out.println("1.Se requiere 80% de asistencia para tener derecho a evaluación parcial y 80% de trabajos en clase.
+\n2.Se permiten 10 minutos de tolerancia y si el alumno llega después de este tiempo puede permanecer en la clase, pero no se tomará la asistencia (Solamente en los horarios de
+inicio:7:00a.m y 14:00 p.m).\n
+\n3. Las faltas deberán estar justificadas mediante el correo institucional con un plazo máximo de 24 horas posteriores a la hora de falta en clase mediante correo del tutor (a),
+justificantes entregados fuera de la fecha límite permitido no se aceptan, únicamente se aceptarán recetas médicas y citatorios jurídicos.(De forma física deben ser presentados al
+tutor para ser validados y de forma posterior emitidos).\n
+\n4. Las tareas y trabajos deberán subirlas al Classroom de forma individual y no se recibirán de manera extemporánea.(Salvo previo justificante validado por el tutor)
+\n4. Las tareas y trabajos deberán subirlas al Classroom de forma individual y no se recibirán de manera extemporánea.(Salvo previo justificante validado por el tutor)
+\n5. Las tareas y trabajos presentarlos en tiempo y forma. La demora de un trabajo o tarea sin justificante y/o con justificante fuera del límite no se aceptan.
+\n6. Utilizar el correo institucional para trabajar bajo la plataforma Google Classroom.
+\n7. El plagio o copia de trabajos y/o exámenes, será condicionado a reprobar a la asignatura y se reportará al área correspondiente.
+\n8. Cualquier deshonestidad académica será sancionada reprobando el parcial sin derecho a examen final
+\n9. En circunstancia de indisciplina o falta de respeto por parte del alumno hacia docentes,administrativos, compañeros o cualquier persona perteneciente a la universidad, se realizará
+una primera llama de atención, si el alumno hace caso omiso tendrá que abandonar el aula, tres incidencias de este tipo el alumno no tendrá derecho a examen final o parcial.
+\n10. Uso de laptops y/o dispositivos móviles quedará limitados a uso exclusivo de las actividades que así lo requieran.
+\n11. Prohibido el uso de audífonos durante la clase.
+\n12. Prohibido comer y/o tomar líquidos en el salón.
+\n13. Prohibido sentarse encima de las mesas , así como columpiarse en las sillas.
+\n14.Todo tema académido debe ser revisado primeramente por parte del alumno con el docente, de no resolverse, pasar con su respectivo tutor, y de ser necesario con la coordinación
+de tutores. En caso de no solucionarse pasar a la dirección del programa educativo (debe mantenerse este seguimiento de forma obligatoria)
+\n15.Cualquier situación no prevista en el presente reglamento pasar directamente con la dirección del programa educativo.
+\n16. El día destinado a entrega de calificaciones todos los estudiantes deben estar presentes, ese día se entregarán exámenes y se brindará retroalimentación
+\n17.Este reglamento entra en vigor después de que se firme o se acepte por la mayoría de los estudiantes asistentes a la primera sesión de la materia, una vez firmado o aceptado por el
+50% más el jefe de grupo, es vigente para todo alumno inscrito en el curso aunque no esté presente en la primera sesión.");
+
+ }
+  static void ReglamentoPOO{
+    System.out.println("Lineamientos Classroom");
+    System.out.println("2.Lineamientos Classroom");
+
+  }
+   
+    
+}
