3a20d185c5d94806c0fcf27d12134a1d86e6fe72
karlaDeniseGuerreroRangel <124050167@upq.edu.mx>
2025-09-23 22:43:48 -0600

    Destructuración.
    Primer ejercicio de la practica.

---
+++ Practica2/persona.js
@@ -0,0 +1,12 @@
+const persona = {
+  nombre: "Karla Denise",
+  edad: 19,
+  ciudad: "Qro",
+  pais: "MX"
+};
+
+
+const { nombre, edad, ciudad } = persona;
+
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
