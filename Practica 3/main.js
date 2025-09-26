89e0a04ba65ef52b7dac4f2c98c93dcc888bf3d9
karlaDeniseGuerreroRangel <124050167@upq.edu.mx>
2025-09-25 23:08:08 -0600

    utils.js
    Primer ejercicio

---
+++ Practica 3/main.js
@@ -0,0 +1,7 @@
+
+import { restar } from './utils.js';
+
+console.log(restar(10, 5));   // Resultado: 5
+console.log(restar(20, 8));   // Resultado: 12
+console.log(restar(0, 3));    // Resultado: -3
+console.log(restar(100, 100)); // Resultado: 0
