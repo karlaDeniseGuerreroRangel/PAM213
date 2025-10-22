52edb5512ad3979b27d47ca84dfafd7859ee0622
karlaDeniseGuerreroRangel <124050167@upq.edu.mx>
2025-10-21 22:48:40 -0600

    Practica No.9: TextInput & Alert

--- my-app/screens/TextInputScreen.js
+++ my-app/screens/TextInputScreen.js
@@ -1,11 +1,89 @@
-import { Text, StyleSheet, View, Button } from 'react-native'
+import React, {useState} from 'react';
+import { View, Text, TextInput, Button, Alert, StyleSheet, Platform  } from "react-native" 
 
-export default function TextInputScreen (){
-    return (
-      <View>
-        <Text>Proximamente por Luis Fernado y sus compañeros</Text>
-      </View>
-    )
+export default function TextInputScreen(){
+ const[nombre, setNombre]= useState('');
+ const[contrasena, setContrasena]= useState('');
+ const[multexto, setMultexto]= useState('');
+
+ const mostrarAlerta = () =>{
+  if (nombre.trim() === ''){
+    if (Platform.OS === 'web'){
+      alert('Por favor escribe tu nombre antes de continuar.');
+    }else{
+      Alert.alert(
+        'Atención',
+        'Por favor escribe tu nombre antes de continuar',
+        [
+          {text: 'cancelar'},
+          {text: 'aceptar'}
+        ]
+      );
+    }
+ }else{
+  if (Platform.OS === 'web'){
+    alert(`Bienvenido, ${nombre}!`);
+  }else {
+    Alert.alert(
+      'Hola', `Bienvenido, ${nombre}!`,
+       [
+          {text: 'cancelar'},
+          {text: 'aceptar'}
+        ]
+    );
   }
+}
+ };
+    return(
+            <View style= {styles.container}> 
+        <Text style= {styles.titulo}>Practica: TextInput y Alert </Text>
 
-const styles = StyleSheet.create({})
+        <TextInput
+        style={styles.input}
+        placeholder="Escribe tu nombre"
+        value={nombre}
+        onChangeText={setNombre}/>
+
+         <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry={true}
+        keyboardType='numeric'
+        value={contrasena}
+        onChangeText={setContrasena}/>
+
+        <TextInput
+        style={styles.input}
+        placeholder="Escribe tu nombre"
+        value={multexto}
+        onChangeText={setMultexto}/>
+
+        <Button title="Mostrar Alerta " onPress={mostrarAlerta}/>
+        </View>
+
+    );
+
+}
+const styles= StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffff',
+    padding: 20,
+  },
+  titulo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+input:{
+  borderWidth: 1,
+  borderColor: '#aaa',
+  borderRadius: 5,
+   width: '80%',
+    padding: 10,
+    marginBottom: 15,
+
+},
+});
