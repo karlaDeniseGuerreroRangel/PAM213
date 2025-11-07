a76e75cb4396d8e774700b5a97e7d30c1a567cdc
karlaDeniseGuerreroRangel <124050167@upq.edu.mx>
2025-11-06 19:53:30 -0600

    1erCommitExamen

---
+++ Examen2doP/App.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, ImageComponent } from 'react-native';
+import { ImageBackground, ImageBackground } from 'react-native-web';
+
+export default function App() {
+   
+  const[screen, setScreen] = useState('menu');
+  const ImageBackground = '...assets/Imagenes/Fondo.jpeg'
+
+  <ImageBackground
+   style={styles}
+
+
+  return(
+    <View style={styles.container}>
+      <Text>Menu Restaurante</Text>
+      <StatusBar style="auto" />
+
+    </View>
+  );
+
+  
+
+
+  
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  const styles = StyleSheet.create(
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  const styles = StyleSheet.create(
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+ 
+});
